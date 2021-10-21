94c963de517db3e36a674ed88ca17ba48ab45743
MeryAmun <chebesiawah@gmail.com>
2021-10-21 02:05:44 +0100

    game almost done

---
+++ sandbox.js
@@ -0,0 +1,70 @@
+
+const button = document.querySelector('.restart');
+const ul = document.querySelector('.tiles1');
+const ul2 = document.querySelector('.tiles2');
+const head = document.querySelector('.header');
+const head2 = document.querySelector('.header2');
+const single = document.getElementsByClassName('.title.singile');
+const tiles = [1,2,3,4,5,6,7,8,9];
+let html = ``;
+tiles.forEach((tile,index) => {
+    const randomColor = "#" + Math.floor(Math.random()*16907215).toString(16) ;
+      html += `<li class="single" tabindex="1" style="background: ${randomColor}">${tile}</li>`; 
+                 
+});
+
+ul.innerHTML = html;
+
+const singleLi = ul.children;
+console.log(singleLi[0].className)
+  // console.log(singleLi[0].innerHTML)
+   //console.log(singleLi[0].style.background)
+
+const keys = Object.keys(ul.children);
+
+keys.forEach((key, index) => {
+  //console.log(key)
+  const singleLi = ul.children;
+  
+  singleLi[key].onclick = border;
+  function border(){
+    singleLi[key].setAttribute('class', 'selected');
+    };
+    singleLi[key].onmouseout =  interval;
+    function interval(){
+      const keys2 = Object.keys(ul.children);
+keys2.forEach((key2, index) => {
+  const singleLi2 = ul2.children;
+  singleLi2[key2].onclick =  change;
+  
+  function change(){
+     singleLi2[key2].style.border = "none";
+    singleLi2[key2].setAttribute('class', 'li2Select');
+   const newClass = singleLi2[key].className
+singleLi[key].setAttribute('class', `${newClass}`);
+singleLi2[key2].append(singleLi[key]);
+
+
+  //  if(singleLi.length < 1 && ul2.innerHTML === [5,4,2,6,1,3,7,8,9] || [1,2.3,6,5,4,7,8,9]){
+  //   head.innerHTML = "Game finished, you won";
+  //  }else 
+  if(singleLi.length < 1 && ul2.innerHTML !== [5,4,2,6,1,3,7,8,9] || [1,2.3,6,5,4,7,8,9]){
+    head.innerHTML = "game finished, you lose";
+  }else{
+    head.innerHTML = `${singleLi.length} moves left`;
+   }
+  }
+ 
+});
+    }
+    
+});
+
+console.log(ul2)
+
+
+button.addEventListener("click", function(){
+  window.location.reload();
+ 
+});
+
